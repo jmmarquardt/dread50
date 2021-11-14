950f975181e0cd0368d5b163520251a336a599a7
John-Michael Marquardt <jmmarquardt@gmail.com>
2021-11-14 15:58:47 -0500

    initial commit

---
+++ Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * This is attached to the FPS controller, and monitors the 
+ * player's y position in comparison to the floor position.
+ * If the Player falls through a hole, the game over scene loads.
+ */
+public class DespawnOnHeight : MonoBehaviour 
+{
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// check if player position is below ground
+		if (transform.GetChild(0).transform.position.y < -1) 
+		{
+			SceneManager.LoadScene("GameOver");
+			DontDestroy.currentLevel = 1;
+		}
+	}
+}
---
+++ Assets/Scripts/LoadSceneOnInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnInput : MonoBehaviour 
+{
+	private Scene currentScene;
+	private Scene titleScene;
+
+	void Start () {
+		currentScene = SceneManager.GetActiveScene();
+		titleScene = SceneManager.GetSceneByName("Title");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentScene = SceneManager.GetActiveScene();
+		
+		if (Input.GetAxis("Submit") == 1) 
+		{
+			if (currentScene == titleScene) 
+			{
+				SceneManager.LoadScene("Play");
+			}
+			else
+			{
+				SceneManager.LoadScene("Title");
+			}
+		}
+	}
+}
